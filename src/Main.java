3e178897c83b975f0c47eff693dc5f3796587c2e
akursus <ak_ursus@list.ru>
2022-12-16 23:53:21 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1
+        System.out.println("Задание 1");
+        printLeapOrNotLeapYear (2000);
+    }
+
+    public static boolean isLeapYear (int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printLeapOrNotLeapYear(int year) {
+        if (isLeapYear (year)) {
+            System.out.println(year + " - год високосный");
+            return;
+        }
+        System.out.println(year + " - год не високосный");
     }
 }
